8f2b3437c63449d50d0bdb8dfdd4dcd62da18d46
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 17:11:27 -0300

    B1: remove unused CRA config module

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-// src/config/config.js
-
-const config = {
-  useBackend: process.env.REACT_APP_USE_BACKEND, // Cambia a 'true' para usar el backend real
-  secretKeyCaptcha: process.env.REACT_APP_SECRET_KEY_CAPTCHA, // Ahora toma la clave correcta
-  clientUri: process.env.REACT_APP_CLIENT_URI, // Agregar esta variable para más flexibilidad
-};
-
-export default config;
