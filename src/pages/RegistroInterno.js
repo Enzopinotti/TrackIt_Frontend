b02ae8ffc5dfada3b9afeddcd9681d97f1a8cf13
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 20:08:52 -0300

    B2: migrate src/pages/RegistroInterno.js to React Router v8 imports

--- src/pages/RegistroInterno.js
+++ src/pages/RegistroInterno.js
@@ -1,6 +1,6 @@
 // src/pages/RegistroInterno.js
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router';
 import Swal from 'sweetalert2';
 import Logo from '../components/Logo.js';
 import { useForm } from 'react-hook-form';
