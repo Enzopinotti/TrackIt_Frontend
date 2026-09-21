853305ee9de0c068598346357b6713c6696b9709
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 20:07:59 -0300

    B2: migrate src/pages/SeleccionRegistro.js to React Router v8 imports

--- src/pages/SeleccionRegistro.js
+++ src/pages/SeleccionRegistro.js
@@ -1,7 +1,7 @@
 // src/pages/SeleccionRegistro.js
 
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router';
 import Logo from '../components/Logo.js';
 
 function SeleccionRegistro() {
