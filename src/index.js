7b332f7abf7e08ac05537231d6204e5d75897f8f
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 17:09:55 -0300

    B1: compile Sass directly through Vite

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import { AuthProvider } from './context/AuthContext.js';
-import './styles/css/main.css';
+import './styles/scss/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
