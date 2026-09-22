c81e559301074c3010bb907d0af895aae0263fde
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 22:00:46 -0300

    B5: make password toggles keyboard accessible in src/pages/RegistroInterno.js

--- src/pages/RegistroInterno.js
+++ src/pages/RegistroInterno.js
@@ -128,7 +128,15 @@ function RegistroInterno() {
               />
               <div
                 onClick={togglePasswordVisibility}
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter' || event.key === ' ') {
+                    event.preventDefault();
+                    togglePasswordVisibility();
+                  }
+                }}
                 className="toggle-password ojo"
+                role="button"
+                tabIndex={0}
                 aria-label="Mostrar u ocultar contraseña"
               >
                 {showPassword ? (
@@ -164,7 +172,15 @@ function RegistroInterno() {
               />
               <div
                 onClick={toggleConfirmPasswordVisibility}
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter' || event.key === ' ') {
+                    event.preventDefault();
+                    toggleConfirmPasswordVisibility();
+                  }
+                }}
                 className="toggle-password ojo"
+                role="button"
+                tabIndex={0}
                 aria-label="Mostrar u ocultar contraseña"
               >
                 {showConfirmPassword ? (
