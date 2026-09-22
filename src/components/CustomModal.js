343a0100da3d9bcd1ad9379b057f6c640ada21c0
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 21:06:00 -0300

    B5: remove ignored function propTypes from src/components/CustomModal.js

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import ReactModal from 'react-modal';
-import PropTypes from 'prop-types';
 
 // Establecer el elemento raíz para accesibilidad
 ReactModal.setAppElement('#root');
@@ -20,11 +19,5 @@ function CustomModal({ isOpen, onRequestClose, children }) {
     </ReactModal>
   );
 }
-
-CustomModal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onRequestClose: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
-};
 
 export default CustomModal;
