e3046e80bc1302b44d8fb1cea7307f4609e1fc48
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 21:18:21 -0300

    B5: remove obsolete React default import from src/pages/SeleccionLogin.js

--- src/pages/SeleccionLogin.js
+++ src/pages/SeleccionLogin.js
@@ -1,6 +1,6 @@
 // src/pages/SeleccionLogin.js
 
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useNavigate, Link } from 'react-router';
 import Logo from '../components/Logo.js';
 import { AuthContext } from '../context/AuthContext.js';
