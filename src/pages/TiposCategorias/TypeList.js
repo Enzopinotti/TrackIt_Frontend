e883c377fb5f6ddb61de7bf851b3ef51beb3722c
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 22:00:51 -0300

    B5: make tipo list selection keyboard accessible

--- src/pages/TiposCategorias/TypeList.js
+++ src/pages/TiposCategorias/TypeList.js
@@ -2,12 +2,25 @@
 
 
 function TypeList({ tipos, onSeleccionar }) {
+  const handleItemKeyDown = (event, id) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      onSeleccionar(id);
+    }
+  };
+
   return (
     <div className="type-list">
       <h2>Lista de Tipos</h2>
       <ul>
         {tipos.map((tipo) => (
-          <li key={tipo.id} onClick={() => onSeleccionar(tipo.id)}>
+          <li
+            key={tipo.id}
+            onClick={() => onSeleccionar(tipo.id)}
+            onKeyDown={(event) => handleItemKeyDown(event, tipo.id)}
+            role="button"
+            tabIndex={0}
+          >
             <div className="info">
               <p className="nombre">{tipo.nombre}</p>
               <p className="cantidad">
