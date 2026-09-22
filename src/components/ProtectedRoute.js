b3388bb5b51ba2a979a76ece8eae8147c0c998bd
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 21:06:07 -0300

    B5: remove ignored function propTypes from src/components/ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,6 @@
 
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router';
-import PropTypes from 'prop-types';
 import { AuthContext } from '../context/AuthContext.js';
 import LoadingOverlay from './LoadingOverlay.js';
 
@@ -19,9 +18,5 @@ function ProtectedRoute({ children }) {
 
   return children;
 }
-
-ProtectedRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default ProtectedRoute;
