d8a6267351dbee0e1e5be2cd44c418e652f9aa47
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 22:00:35 -0300

    B5: make logo navigation keyboard accessible

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,8 +7,23 @@ const Logo = () => {
     navigate('/home');
   };
 
+  const handleLogoKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      handleLogoClick();
+    }
+  };
+
   return (
-    <div className='logoContainer' onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+    <div
+      className='logoContainer'
+      onClick={handleLogoClick}
+      onKeyDown={handleLogoKeyDown}
+      role="button"
+      tabIndex={0}
+      aria-label="Ir al inicio"
+      style={{ cursor: 'pointer' }}
+    >
       <img src="/assets/images/logo.png" alt="Logo" />
     </div>
   );
