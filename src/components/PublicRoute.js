ff8c24d86a4e58c0e90a4f6c82e64aef8980b182
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-21 21:16:00 -0300

    B5: remove obsolete React default import from src/components/PublicRoute.js

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,6 +1,6 @@
 // src/components/PublicRoute.js
 
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Navigate } from 'react-router';
 import { AuthContext } from '../context/AuthContext.js';
 import LoadingOverlay from './LoadingOverlay.js';
